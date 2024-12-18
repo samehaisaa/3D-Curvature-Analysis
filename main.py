03b00a4fa01559c14b59fa821cb29103b9826cfe
samehaisaa <a.semeh555@gmail.com>
2024-12-18 17:54:14 +0000

    changed to main to vtdk format

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from courbure.gauss import CourbureGauss
 from courbure.mean import CourbureMoyenne
 from courbure.max_curvature import CourbureMax
 from courbure.min_cruvature import CourbureMin
-from utils.visualize import visualize
+from utils.visualize import visualize_curvature
 import trimesh
 
 def main():
@@ -23,24 +23,25 @@ def main():
 
         if choix == "1":
             courbure_gauss = CourbureGauss.calculer(mesh)
-            print("Courbure de Gauss calculée.")
-            visualize(mesh, courbure_gauss, title="Courbure de Gauss")
+            print("Courbure de Gauss calculée.",courbure_gauss)
+            print("courbure gauss shape : ", courbure_gauss.shape)
+            visualize_curvature(mesh, courbure_gauss, title="Courbure de Gauss")
         elif choix == "2":
             courbure_moyenne = CourbureMoyenne.calculer(mesh)
             print("Courbure Moyenne calculée.")
-            visualize(mesh, courbure_moyenne, title="Courbure Moyenne")
+            visualize_curvature(mesh, courbure_moyenne, title="Courbure Moyenne")
         elif choix == "3":
             courbure_gauss = CourbureGauss.calculer(mesh)
             courbure_moyenne = CourbureMoyenne.calculer(mesh)
             courbure_max = CourbureMax.calculer(courbure_moyenne, courbure_gauss)
             print("Courbure Maximale calculée.")
-            visualize(mesh, courbure_max, title="Courbure Maximale")
+            visualize_curvature(mesh, courbure_max, title="Courbure Maximale")
         elif choix == "4":
             courbure_gauss = CourbureGauss.calculer(mesh)
             courbure_moyenne = CourbureMoyenne.calculer(mesh)
             courbure_min = CourbureMin.calculer(courbure_moyenne, courbure_gauss)
             print("Courbure Minimale calculée.")
-            visualize(mesh, courbure_min, title="Courbure Minimale")
+            visualize_curvature(mesh, courbure_min, title="Courbure Minimale")
         elif choix == "5":
             print("Quitter le programme. À bientôt!")
             break
