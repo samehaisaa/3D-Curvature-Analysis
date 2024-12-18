3f08801be3dab8e51f1810dd5178b805a1722f3b
samehaisaa <a.semeh555@gmail.com>
2024-12-18 18:26:56 +0000

    approximated surface normals

--- courbure/mean.py
+++ courbure/mean.py
@@ -1,30 +1,22 @@
 import numpy as np
 
-class CourbureMoyenne:
-    @staticmethod
-    def calculer(mesh):
-        # Get the vertex normals (approximated surface normals)
-        vertex_normals = mesh.vertex_normals
+from trimesh import curvature
 
-        # Get the face areas
-        face_areas = mesh.area_faces
-        
-        # Initialize vertex area sums and mean curvature values
-        vertex_areas = np.zeros(len(mesh.vertices))
-        mean_curvatures = np.zeros(len(mesh.vertices))
-
-        # Distribute face areas to vertices (Weighted average)
-        for i, face in enumerate(mesh.faces):
-            for vertex in face:
-                vertex_areas[vertex] += face_areas[i] / 3.0
-
-        # Calculate mean curvature by the change in normals
-        for i, face in enumerate(mesh.faces):
-            vertices = mesh.vertices[mesh.faces[i]]
-            for vertex in face:
-                # The mean curvature for each vertex can be approximated
-                # by the difference of normals of neighboring faces
-                normal_diff = np.linalg.norm(vertex_normals[face[0]] - vertex_normals[face[1]])  # Example diff, modify for neighbors
-                mean_curvatures[vertex] += normal_diff / vertex_areas[vertex]  # Weight by area
-        
-        return mean_curvatures
+def calculate_mean_curvature(mesh, radius=0.1):
+    """
+    Calculate the mean curvature of the mesh using the method from the `trimesh` repository.
+    
+    Parameters:
+    - mesh: Trimesh object containing the 3D mesh.
+    - radius: The radius around each point for calculating curvature (default is 0.1).
+    
+    Returns:
+    - mean_curvature: A numpy array of mean curvature values for each vertex in the mesh.
+    """
+    # Get the points (vertices) of the mesh
+    points = mesh.vertices
+    
+    # Calculate the mean curvature using the method provided in the trimesh repo
+    mean_curvature = curvature.discrete_mean_curvature_measure(mesh, points, radius)
+    
+    return mean_curvature
